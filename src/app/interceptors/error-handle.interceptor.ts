2e1180dd96e20c0ef67907d28b427bab230a4717
adrian <fak.adrian@gmail.com>
2021-06-16 10:01:03 +0200

    add error handle interceptor

---
+++ src/app/interceptors/error-handle.interceptor.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ToastrService } from 'ngx-toastr';
+
+@Injectable()
+export class ErrorHandleInterceptor implements HttpInterceptor {
+
+  constructor(private toastr: ToastrService) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError(error => {
+        const errors  = error.error.errors
+        if(errors?.length > 0){
+          errors.forEach((err: any)=>{
+            this.toastr.error(err?.name, 'Error' );
+          })
+          
+        }
+        return throwError(errors)
+      })
+    )
+  }
+}
