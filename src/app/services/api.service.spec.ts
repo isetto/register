0530a58c2c16ecda3cade9619b7ffe6b83d74338
adrian <fak.adrian@gmail.com>
2021-06-16 13:06:22 +0200

    test apiService

--- src/app/services/api.service.spec.ts
+++ src/app/services/api.service.spec.ts
@@ -1,16 +1,62 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ApiService } from './api.service';
+import { User } from '../model/user';
 
-describe('ApiService', () => {
-  let service: ApiService;
+describe( 'ApiService', () => {
+  let apiService: ApiService;
+  let httpTestingController: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ApiService);
-  });
+  beforeEach( () => {
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+    TestBed.configureTestingModule( {
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        ApiService
+      ]
+    } );
+
+    apiService = TestBed.inject( ApiService ),
+      httpTestingController = TestBed.inject( HttpTestingController );
+
+  } );
+
+
+  it( 'should be created', () => {
+    expect( apiService ).toBeTruthy();
+  } );
+
+  fit( 'should save the user data and return it', () => {
+
+    const user: User = {
+      firstName: 'Adrian',
+      lastName: 'Fąk',
+      email: 'test@wp.pl'
+    }
+
+    apiService.register( user )
+      .subscribe( ( userData: User ) => {
+        expect( userData.firstName ).toEqual( user.firstName );
+        expect( userData.lastName ).toEqual( user.lastName );
+        expect( userData.email ).toEqual( user.email );
+        expect( userData._id ).toBeTruthy()
+      } );
+
+    const req = httpTestingController.expectOne( 'https://demo-api.now.sh/users' );
+
+    expect( req.request.method ).toEqual( "POST" );
+
+    req.flush( {
+      _id: '23qwesd',
+      ...user
+    } )
+
+  } );
+
+  afterEach( () => {
+    httpTestingController.verify();
+  } );
+
+} );
